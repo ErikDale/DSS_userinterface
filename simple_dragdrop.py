db1474959506c312f2f19b0c6aeb2c7821c37ab2
ErikDale <erikdale@stud.ntnu.no>
2022-03-06 17:34:05 +0100

    Put all the code into one class and created a label that displays the filename of the image being displayed

--- simple_dragdrop.py
+++ simple_dragdrop.py
@@ -6,47 +6,80 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 
 
-class DropZone(QLabel):
+class App(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("DSS classifier")
+        self.setGeometry(0, 0, 800, 800)
+        self.setAcceptDrops(True)
+
+        # Creates a QVBoxLayout
+        self.grid = QGridLayout()
+
+        # Creates a label that should contain the name of the file uploaded
+        self.fileNameLabel = QLabel()
+        self.fileNameLabel.setAlignment(Qt.AlignCenter)
+        self.grid.addWidget(self.fileNameLabel, 0, 0, 1, 2)
+
+        # Creates a label that will act as the photoviewer
+        self.photoViewer = QLabel()
         # Sets the alignment of the text to center
-        self.setAlignment(Qt.AlignCenter)
-        self.setText('\n\n Drop DSS Image Here \n\n')
+        self.photoViewer.setAlignment(Qt.AlignCenter)
+        self.photoViewer.setText('\n\n Drop DSS Image Here \n\n')
 
         # Sets a border around the label
-        self.setStyleSheet('''
-            QLabel{
-                border: 4px dashed #aaa
-            }
-        ''')
+        self.photoViewer.setStyleSheet('''
+                    QLabel{
+                        border: 4px dashed #aaa
+                    }
+                ''')
 
-    # Method that removes the Image from the drop zone
-    def removeImage(self):
-        # Checks if there actually is an image to remove or not
-        if super().pixmap() is None:
-            # Takes away the borders so that the message box wont contain weird borders
-            self.setStyleSheet('''
-                        QLabel{
-                            border: None
-                        }
-                    ''')
-            # A message box will appear telling the user that there is no image displayed
-            msg = QtWidgets.QMessageBox()
-            msg.information(self, "No Image Displayed", "There is no image displayed")
-            # Puts the borders back again
-            self.setStyleSheet('''
-                        QLabel{
-                            border: 4px dashed #aaa
-                        }
-                    ''')
-        else:
-            self.setAlignment(Qt.AlignCenter)
-            self.setText('\n\n Drop DSS Image Here \n\n')
-            self.setStyleSheet('''
-                        QLabel{
-                            border: 4px dashed #aaa
-                        }
-                    ''')
+        self.grid.addWidget(self.photoViewer, 1, 0, 1, 2)
+
+        # Creates a button for opening the file explorer to upload an image
+        self.browseButton = QtWidgets.QPushButton(self)
+        self.browseButton.setText("Browse Images")
+        self.browseButton.clicked.connect(self.explore)
+
+        # Puts the button in the grid
+        self.grid.addWidget(self.browseButton, 2, 0)
+
+        # Creates a button for removing the image
+        self.removeButton = QtWidgets.QPushButton(self)
+        self.removeButton.setText("Remove Image")
+        self.removeButton.clicked.connect(self.removeImage)
+
+        # Puts the button in the grid
+        self.grid.addWidget(self.removeButton, 2, 1)
+
+        self.emptyLabel = QLabel("---------------------------------------------------------"
+                                 "---------------------------------------------------------"
+                                 "---------------------------------------------------------")
+        self.emptyLabel.setAlignment(Qt.AlignCenter)
+        self.grid.addWidget(self.emptyLabel, 3, 0, 1, 2)
+
+        self.grid.setRowStretch(1, 18)
+        self.grid.setRowStretch(2, 1)
+        self.grid.setRowStretch(3, 1)
+        self.grid.setRowStretch(4, 1)
+
+        # Creates a button for cropping the image
+        self.cropButton = QtWidgets.QPushButton(self)
+        self.cropButton.setText("Crop Image")
+        # self.removeButton.clicked.connect(<some_function>)
+
+        # Puts the button in the grid
+        self.grid.addWidget(self.cropButton, 4, 0)
+
+        # Creates a button for classifying the image
+        self.classifyButton = QtWidgets.QPushButton(self)
+        self.classifyButton.setText("Classify Image")
+        # self.removeButton.clicked.connect(<some_function>)
+
+        # Puts the button in the grid
+        self.grid.addWidget(self.classifyButton, 4, 1)
+
+        self.setLayout(self.grid)
 
     # Method to open file explorer and choose an image
     def explore(self):
@@ -63,70 +96,41 @@ class DropZone(QLabel):
         else:
             fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', str(path),
                                                           'JPG files (*.jpg);;PNG files (*.png)')
-        super().setPixmap(QPixmap(fname[0]))
+        # Displays the image in the photoViewer label
+        self.set_image(fname[0])
 
-    def setPixmap(self, image):
-        super().setPixmap(image)
+        # Fetches the filename from the path and sets it as the header
+        pathList = str(fname[0]).split("/")
+        self.fileNameLabel.setText("Filename: " + pathList[-1])
 
-
-class AppDemo(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("DSS classifier")
-        self.setGeometry(0, 0, 800, 800)
-        self.setAcceptDrops(True)
-
-        # Creates a QVBoxLayout
-        self.grid = QGridLayout()
-
-        # Creates an instance of the DropZone class
-        self.photoViewer = DropZone()
-        self.grid.addWidget(self.photoViewer, 0, 0, 1, 2)
-
-        # Creates a button for opening the file explorer to upload an image
-        self.browseButton = QtWidgets.QPushButton(self)
-        self.browseButton.setText("Browse Images")
-        self.browseButton.clicked.connect(self.photoViewer.explore)
-
-        # Puts the button in the grid
-        self.grid.addWidget(self.browseButton, 1, 0)
-
-        # Creates a button for removing the image
-        self.removeButton = QtWidgets.QPushButton(self)
-        self.removeButton.setText("Remove Image")
-        self.removeButton.clicked.connect(self.photoViewer.removeImage)
-
-        # Puts the button in the grid
-        self.grid.addWidget(self.removeButton, 1, 1)
-
-        self.label = QLabel("---------------------------------------------------------"
-                            "---------------------------------------------------------"
-                            "---------------------------------------------------------")
-        self.label.setAlignment(Qt.AlignCenter)
-        self.grid.addWidget(self.label, 2, 0, 1, 2)
-
-        self.grid.setRowStretch(0, 18)
-        self.grid.setRowStretch(1, 1)
-        self.grid.setRowStretch(2, 1)
-        self.grid.setRowStretch(3, 1)
-
-        # Creates a button for cropping the image
-        self.cropButton = QtWidgets.QPushButton(self)
-        self.cropButton.setText("Crop Image")
-        # self.removeButton.clicked.connect(<some_function>)
-
-        # Puts the button in the grid
-        self.grid.addWidget(self.cropButton, 3, 0)
-
-        # Creates a button for classifying the image
-        self.classifyButton = QtWidgets.QPushButton(self)
-        self.classifyButton.setText("Classify Image")
-        # self.removeButton.clicked.connect(<some_function>)
-
-        # Puts the button in the grid
-        self.grid.addWidget(self.classifyButton, 3, 1)
-
-        self.setLayout(self.grid)
+    # Method that removes the Image from the drop zone
+    def removeImage(self):
+        # Checks if there actually is an image to remove or not
+        if QLabel.pixmap(self.photoViewer) is None:
+            # Takes away the borders so that the message box wont contain weird borders
+            self.photoViewer.setStyleSheet('''
+                        QLabel{
+                            border: None
+                        }
+                    ''')
+            # A message box will appear telling the user that there is no image displayed
+            msg = QtWidgets.QMessageBox()
+            msg.information(self.photoViewer, "No Image Displayed", "There is no image displayed")
+            # Puts the borders back again
+            self.photoViewer.setStyleSheet('''
+                        QLabel{
+                            border: 4px dashed #aaa
+                        }
+                    ''')
+        else:
+            self.fileNameLabel.setText("")
+            self.photoViewer.setAlignment(Qt.AlignCenter)
+            self.photoViewer.setText('\n\n Drop DSS Image Here \n\n')
+            self.photoViewer.setStyleSheet('''
+                        QLabel{
+                            border: 4px dashed #aaa
+                        }
+                    ''')
 
     # Checks if the file that enters the drop zone is an image
     def dragEnterEvent(self, event):
@@ -153,7 +157,13 @@ class AppDemo(QWidget):
             if event.mimeData().hasImage:
                 event.setDropAction(Qt.CopyAction)
                 file_path = event.mimeData().urls()[0].toLocalFile()
+
+                # Displays the image in the photoViewer label
                 self.set_image(file_path)
+
+                # Fetches the filename from the path and sets it as the header
+                pathList = str(file_path).split("/")
+                self.fileNameLabel.setText("Filename: " + pathList[-1])
                 event.accept()
             else:
                 event.ignore()
@@ -169,6 +179,6 @@ class AppDemo(QWidget):
 
 
 app = QApplication(sys.argv)
-demo = AppDemo()
+demo = App()
 demo.show()
 sys.exit(app.exec_())
