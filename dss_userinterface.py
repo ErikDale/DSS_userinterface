f9624faca57fdd11d6e0ce8ece470502eae2e5cf
ErikDale <erikdale@stud.ntnu.no>
2022-03-08 23:38:59 +0100

    Made it so that when the user crops an image the crop stays at the same position. Also added an uncrop button to undo the crop

--- dss_userinterface.py
+++ dss_userinterface.py
@@ -4,7 +4,7 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QRubberBand, QSizePolicy, QScrollArea, \
     QMainWindow, QVBoxLayout, QAction, QShortcut, QGraphicsView
 from PyQt5.QtCore import Qt, QRect, QSize, QPoint, pyqtSignal
-from PyQt5.QtGui import QPixmap, QImage, QKeySequence, QFont
+from PyQt5.QtGui import QPixmap, QImage, QKeySequence, QFont, QPainter, QBrush, QPalette
 
 
 # Gotten alot from: https://stackoverflow.com/questions/35508711/how-to-enable-pan-and-zoom-in-a-qgraphicsview
@@ -37,6 +37,7 @@ class PhotoViewer(QtWidgets.QGraphicsView):
         self.rubberBandItem = QtWidgets.QRubberBand(
             QtWidgets.QRubberBand.Rectangle
         )
+        self.rubberBandItemGeometry = None
         item = self.scene.addWidget(self.rubberBandItem)
         item.setZValue(-1)
         self.draggable = False
@@ -44,8 +45,13 @@ class PhotoViewer(QtWidgets.QGraphicsView):
         self.origin = QtCore.QPoint()
         self.rubberBool = False
 
+        self.rectangle = None
+
+        self.isCropped = False
+
     # Method for removing image from pixmap
     def removeItem(self):
+        self.scene.removeItem(self.rectangle)
         pixmap = QPixmap()
         self.photo.setPixmap(pixmap)
 
@@ -54,12 +60,43 @@ class PhotoViewer(QtWidgets.QGraphicsView):
         return not self.empty
 
     # Method to set a photo in the pixmap
+    def setPhotoWithRectangle(self, rectangle=True, pixmap=None):
+        if rectangle:
+            self.zoom = 0
+            if pixmap and not pixmap.isNull():
+                self.empty = False
+                self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+                # Creates a rectangle and adds it to the background of the image
+                self.rectangle = QtWidgets.QGraphicsRectItem(QtCore.QRectF(0, 0, pixmap.width(), pixmap.height()))
+                self.scene.addItem(self.rectangle)
+                # Giving color to the rectangle
+                self.rectangle.setBrush(Qt.white)
+                self.rectangle.setZValue(-1)
+                self.photo.setPixmap(pixmap)
+            else:
+                self.empty = True
+                self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+                self.photo.setPixmap(QtGui.QPixmap())
+        else:
+            self.zoom = 0
+            if pixmap and not pixmap.isNull():
+                self.empty = False
+                self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+                self.photo.setPixmap(pixmap)
+            else:
+                self.empty = True
+                self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+                self.photo.setPixmap(QtGui.QPixmap())
+
+    # Method to set a photo in the pixmap
     def setPhoto(self, pixmap=None):
         self.zoom = 0
         if pixmap and not pixmap.isNull():
             self.empty = False
             self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
             self.photo.setPixmap(pixmap)
+            # Setting the image at the exact location it was cropped, to keep its coordinates
+            self.photo.setPos(self.rubberBandItemGeometry.x(), self.rubberBandItemGeometry.y())
         else:
             self.empty = True
             self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
@@ -69,7 +106,7 @@ class PhotoViewer(QtWidgets.QGraphicsView):
     def wheelEvent(self, event):
         if self.hasPhoto():
             if event.angleDelta().y() > 0:
-                factor = 1.25
+                factor = 1.20
                 self.zoom += 1
             else:
                 factor = 0.8
@@ -124,10 +161,13 @@ class PhotoViewer(QtWidgets.QGraphicsView):
                 QtCore.QRect(self.origin, end_pos).normalized()
             )
             self.draggable = False
-            crop = self.photo.pixmap().copy(self.rubberBandItem.geometry())
+            # Getting the cropped area and storing it in the pixmap
+            self.rubberBandItemGeometry = self.rubberBandItem.geometry()
+            crop = self.photo.pixmap().copy(self.rubberBandItemGeometry)
             self.setPhoto(crop)
             self.rubberBandItem.hide()
             self.rubberBool = False
+            self.isCropped = True
         elif not self.photo.pixmap().isNull() and not self.rubberBool:
             self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
         super(PhotoViewer, self).mouseMoveEvent(event)
@@ -139,19 +179,12 @@ class App(QWidget):
         super().__init__()
         # Sets the style, title and shape of the application
         self.setStyleSheet("QLabel{font-size: 12pt;}")
-        self.setWindowTitle("DSS classifier")
+        self.setWindowTitle("DSS Classifier")
         self.setGeometry(0, 0, 1200, 800)
         self.setAcceptDrops(True)
 
         # Creates a QGridLayout
         self.grid = QGridLayout()
-
-        # Creates a help button that opens a help menu for the user
-        self.helpButton = QtWidgets.QPushButton()
-        self.helpButton.setText("Help")
-        self.helpButton.setFont(QFont('Arial', 12))
-        self.helpButton.clicked.connect(self.helpBox)
-        self.grid.addWidget(self.helpButton, 0, 0, 1, 2)
 
         # Creates a label that should contain the name of the file uploaded
         self.fileNameLabel = QLabel()
@@ -163,22 +196,23 @@ class App(QWidget):
                                         border: 4px dashed #aaa
                                     }
                                 ''')
-        self.grid.addWidget(self.fileNameLabel, 1, 0, 1, 2)
+
+        self.grid.addWidget(self.fileNameLabel, 0, 0, 1, 2)
 
         # Creates an instance of the PhotoViewer class
         self.photoViewer = PhotoViewer(self)
 
         # Adds the photoViewer in the grid
-        self.grid.addWidget(self.photoViewer, 2, 0, 1, 2)
+        self.grid.addWidget(self.photoViewer, 1, 0, 1, 2)
 
         # Creates a button for opening the file explorer to upload an image
         self.browseButton = QtWidgets.QPushButton(self)
-        self.browseButton.setText("Browse Images")
+        self.browseButton.setText("Open Images")
         self.browseButton.clicked.connect(self.explore)
         self.browseButton.setFont(QFont('Arial', 12))
 
         # Puts the button in the grid
-        self.grid.addWidget(self.browseButton, 3, 0)
+        self.grid.addWidget(self.browseButton, 2, 0)
 
         # Creates a button for removing the image
         self.removeButton = QtWidgets.QPushButton(self)
@@ -187,18 +221,28 @@ class App(QWidget):
         self.removeButton.setFont(QFont('Arial', 12))
 
         # Puts the button in the grid
-        self.grid.addWidget(self.removeButton, 3, 1)
+        self.grid.addWidget(self.removeButton, 2, 1)
 
-        self.emptyLabel = QLabel("---------------------------------------------------------"
-                                 "---------------------------------------------------------"
-                                 "---------------------------------------------------------")
+        # Creates an empty label
+        self.emptyLabel = QLabel()
         self.emptyLabel.setAlignment(Qt.AlignCenter)
-        self.grid.addWidget(self.emptyLabel, 4, 0, 1, 2)
+        self.emptyLabel.setStyleSheet('''
+                                    QLabel{
+                                        border: 4px dashed #aaa
+                                    }
+                                ''')
 
-        self.grid.setRowStretch(1, 3)
-        self.grid.setRowStretch(2, 18)
+        self.uncropButton = QtWidgets.QPushButton()
+        self.uncropButton.setText("Uncrop Image")
+        self.uncropButton.setFont(QFont('Arial', 12))
+        self.uncropButton.clicked.connect(self.uncropImage)
+
+        self.grid.addWidget(self.uncropButton, 3, 1)
+
+        self.grid.setRowStretch(0, 3)
+        self.grid.setRowStretch(1, 18)
+        self.grid.setRowStretch(2, 1)
         self.grid.setRowStretch(3, 1)
-        self.grid.setRowStretch(4, 1)
 
         # Creates a button for cropping the image
         self.cropButton = QtWidgets.QPushButton(self)
@@ -207,27 +251,52 @@ class App(QWidget):
         self.cropButton.setFont(QFont('Arial', 12))
 
         # Puts the button in the grid
-        self.grid.addWidget(self.cropButton, 5, 0)
+        self.grid.addWidget(self.cropButton, 3, 0)
 
         # Creates a button for classifying the image
         self.classifyButton = QtWidgets.QPushButton(self)
         self.classifyButton.setText("Classify Image")
-        # self.removeButton.clicked.connect(<some_function>)
+        self.removeButton.clicked.connect(self.removeImage)
         self.classifyButton.setFont(QFont('Arial', 12))
 
         # Puts the button in the grid
-        self.grid.addWidget(self.classifyButton, 5, 1)
+        self.grid.addWidget(self.classifyButton, 4, 0)
+
+        # Creates a help button that opens a help menu for the user
+        self.helpButton = QtWidgets.QPushButton()
+        self.helpButton.setText("Help")
+        self.helpButton.setFont(QFont('Arial', 12))
+        self.helpButton.clicked.connect(self.helpBox)
+        self.grid.addWidget(self.helpButton, 4, 1)
 
         self.setLayout(self.grid)
 
         self.createShortCuts()
+
+        self.imagePath = None
+
+    # Methos that makes the user able to uncrop an image
+    def uncropImage(self):
+        # Checks if there actually is an image to crop or not
+        if self.photoViewer.empty is True:
+            # A message box will appear telling the user that there is no image displayed
+            msg = QtWidgets.QMessageBox()
+            msg.information(self.photoViewer, "No Image Displayed", "There is no image displayed")
+        elif not self.photoViewer.isCropped:
+            # A message box will appear telling the user that the image is not cropped
+            msg = QtWidgets.QMessageBox()
+            msg.information(self.photoViewer, "No Crop", "The image has not been cropped yet")
+        else:
+            self.photoViewer.photo.setPos(0, 0)
+            self.photoViewer.setPhotoWithRectangle(pixmap=QPixmap(self.imagePath), rectangle=False)
+            self.photoViewer.isCropped = False
 
     # Method that displays a help box to the user
     def helpBox(self):
         # A message box will appear telling the user that there is no image displayed
         msg = QtWidgets.QMessageBox()
         msg.information(self, "Help", "Shortcuts: \n-Exit app: Ctrl+Q\n-Open images: Ctrl+O\n-Remove image: "
-                                      "Ctrl+R\n-Crop image: Ctrl+W\n-Open help menu: Ctrl+H")
+                                      "Ctrl+R\n-Crop image: Ctrl+W\n-Open help menu: Ctrl+H\n-Uncrop image: Ctrl+U")
 
     # Method that creates shortcuts for the user
     def createShortCuts(self):
@@ -245,6 +314,9 @@ class App(QWidget):
 
         helpShort = QShortcut(QKeySequence("Ctrl+H"), self)
         helpShort.activated.connect(self.helpBox)
+
+        uncropShort = QShortcut(QKeySequence("Ctrl+U"), self)
+        uncropShort.activated.connect(self.uncropImage)
 
     # Method that sets rubberBool to true, so that the rubberBand is shown
     def rubberBandOn(self):
@@ -265,21 +337,22 @@ class App(QWidget):
         # Checks if the computer has a directory called %HOMEPATH%\Pictures
         if os.path.exists(pathStr):
             # Opens the file explorer in
-            fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', pathStr,
+            fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', pathStr,
                                                           'JPG files (*.jpg);;PNG files (*.png)')
         # If the computer doesn't have a file like that it will open the file explorer in the %HOMEPATH%
         else:
-            fname = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', str(path),
+            fileName = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', str(path),
                                                           'JPG files (*.jpg);;PNG files (*.png)')
 
         # Check if the user has specified a path or just closed the file explorer
-        if fname[0] != "":
+        if fileName[0] != "":
+            self.imagePath = fileName[0]
             # Displays the image in the photoViewer label
-            # self.set_image(fname[0])
-            self.set_image(fname[0])
-
+            self.photoViewer.setPhotoWithRectangle(pixmap=QPixmap(fileName[0]))
+            # Setting the position of the image to the upper right corner of the pixmap
+            self.photoViewer.photo.setPos(0, 0)
             # Fetches the filename from the path and sets it as the header
-            pathList = str(fname[0]).split("/")
+            pathList = str(fileName[0]).split("/")
             self.fileNameLabel.setText("Filename: " + pathList[-1])
 
     # Method that removes the Image from the drop zone
@@ -296,6 +369,8 @@ class App(QWidget):
             self.photoViewer.removeItem()
             # Sets ruberBool to false so that you cannot crop when no image is displayed
             self.photoViewer.rubberBool = False
+            # Setting the position of the image to the upper right corner of the pixmap
+            self.photoViewer.photo.setPos(0, 0)
 
     # Checks if the file that enters the drop zone is an image
     def dragEnterEvent(self, event):
@@ -314,20 +389,21 @@ class App(QWidget):
     # Method that is run when you drop an image in the drop zone
     def dropEvent(self, event):
         # Gets the file extension of the chosen file
-        file_path = event.mimeData().urls()[0].toLocalFile()
-        _, extension = os.path.splitext(file_path)
+        filePath = event.mimeData().urls()[0].toLocalFile()
+        _, extension = os.path.splitext(filePath)
         # If the file is not of type png or jpg an error message will apear
         if extension == ".png" or extension == ".PNG" or extension == ".JPG" or extension == ".jpg" or \
                 extension == ".JPEG" or extension == ".jpeg":
             if event.mimeData().hasImage:
                 event.setDropAction(Qt.CopyAction)
-                file_path = event.mimeData().urls()[0].toLocalFile()
-
+                filePath = event.mimeData().urls()[0].toLocalFile()
+                self.imagePath = filePath
                 # Displays the image in the photoViewer label
-                self.set_image(file_path)
-
+                self.photoViewer.setPhotoWithRectangle(pixmap=QPixmap(filePath))
+                # Setting the position of the image to the upper right corner of the pixmap
+                self.photoViewer.photo.setPos(0, 0)
                 # Fetches the filename from the path and sets it as the header
-                pathList = str(file_path).split("/")
+                pathList = str(filePath).split("/")
                 self.fileNameLabel.setText("Filename: " + pathList[-1])
                 event.accept()
             else:
@@ -337,10 +413,6 @@ class App(QWidget):
             # A message box will appear telling the user that the file type must be jpg or png
             msg = QtWidgets.QMessageBox()
             msg.information(self, "Wrong File Type", "The file must be of type jpg or png")
-
-    # Methods to set image in the photoViewer
-    def set_image(self, img_path):
-        self.photoViewer.setPhoto(QPixmap(img_path))
 
 
 # Starts the application
